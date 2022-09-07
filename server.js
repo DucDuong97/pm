be2febee6cf5cec5ff408a1469ef3dd8220b8320
DESKTOP-CDO0SQ2\Admin <duc.duong@platform.inc>
2022-09-07 15:37:49 +0700

    ...

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 
 const { startWorkers, stopWorker, getWorkersList, getWorkerData } = require('./pm-control.js');
-const { app_worker_config, app_workers_config } = require('./platform.js');
+const { app_worker_config } = require('./platform.js');
 
 require('dotenv').config({path:__dirname+`/.env`});
 
@@ -8,19 +8,19 @@ const app = require("express")();
 
 app.use(require("body-parser").urlencoded({ extended: false }));
 
-app.get("/url", (req, res, next) => {
+app.get("/url", (req, res) => {
     res.json(["Tony","Lisa","Michael","Ginger","Food"]);
 });
 
-app.post("/start-worker", (req, res, next) => {
-    console.log(req.body);
+app.post("/start-worker", (req, res) => {
+
     let app_name = req.body.app_name;
-    let worker_name = req.body.worker_name;
-    let amount = req.body.amount;
+    let worker   = req.body.worker_name;
+    let amount   = req.body.amount;
 
     let result = {};
 
-    startWorkers(app_worker_config(app_name, worker_name, amount), (err, apps) => {
+    startWorkers(app_worker_config(app_name, worker, amount), (err, apps) => {
         if (err) {
             result.success = false;
             result.err_msg = err;
@@ -33,13 +33,14 @@ app.post("/start-worker", (req, res, next) => {
     });
 });
 
-app.post("/stop-worker", (req, res, next) => {
+app.post("/stop-worker", (req, res) => {
+
     let app_name = req.query.app_name;
-    let worker_name = req.query.worker_name;
+    let worker   = req.query.worker_name;
 
     let result = {};
 
-    stopWorker(app_name + '.' + worker_name, (err, apps) => {
+    stopWorker(app_name + '.' + worker, (err, apps) => {
         if (err) {
             result.success = false;
             result.err_msg = err;
@@ -51,7 +52,8 @@ app.post("/stop-worker", (req, res, next) => {
     });
 });
 
-app.get("/describe-worker/:worker_name", (req, res, next) => {
+app.get("/describe-worker/:worker_name", (req, res) => {
+
     let result = {};
 
     getWorkerData(req.params.worker_name, (err, worker) => {
@@ -63,12 +65,11 @@ app.get("/describe-worker/:worker_name", (req, res, next) => {
             result.amount = worker.length;
             result.worker = worker;
         }
-
         res.json(result);
     });
 });
 
-app.get("/list", (req, res, next) => {
+app.get("/list", (req, res) => {
     getWorkersList((err,list) => res.json(list));
 });
 
