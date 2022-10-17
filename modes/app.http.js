d6911a4ca93e6dc8388a679fab5e03dce397b262
duc <duc.duong@platform.inc>
2022-10-17 17:35:03 +0700

    create network

--- modes/app.http.js
+++ modes/app.http.js
@@ -1,6 +1,4 @@
 
-require('dotenv').config({path:require("path").dirname(__dirname)+`/.env${process.env.NODE_ENV == 'development' ? '':'.local'}`});
-
 const axios = require('axios');
 const https = require('https');
 const fs = require('fs');
@@ -14,7 +12,7 @@ const httpsAgent = new https.Agent({
 })
 
 const execute = function(job_context, dataCb, errCb, succCb, failCb, finalCb){
-    console.log('Sending HTTP...');
+    console.log('Sending HTTP: '+`${process.env.HTTP_URL}/${job_context.app}/.queue/${job_context.worker}`);
 
     var bodyFormData = new FormData();
     bodyFormData.append('__utoken', process.env.UTOKEN);
@@ -29,6 +27,14 @@ const execute = function(job_context, dataCb, errCb, succCb, failCb, finalCb){
     .then(res => {
         const data = res.data;
         console.log('Data: ', data);
+
+        if (typeof data === 'str'){
+            console.log("[x] Uncaught error");
+            errCb(data);
+            failCb();
+            finalCb();
+        }
+
         dataCb(data.data);
         if (data && data.code == 1){
             succCb();
